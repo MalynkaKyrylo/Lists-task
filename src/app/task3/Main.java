f348bac6f85e5d649d306d14f5c11c35c8db9567
Malynka Kyrylo <kirillmalinka@gmail.com>
2023-07-24 23:11:01 +0300

    Solution Task 3

--- src/app/task3/Main.java
+++ src/app/task3/Main.java
@@ -1,7 +1,41 @@
 package app.task3;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        LinkedList<String> list = getData();
+        printInitialList(list);
+        printEditedList(list);
+    }
 
+    private static LinkedList<String> getData() {
+        return new LinkedList<>(List.of("orange", "grape", "apple", "lemon"));
+    }
+
+    private static void printInitialList(LinkedList<String> list) {
+        System.out.println("Initial list:");
+        getOutput(list);
+    }
+
+    private static void addData(LinkedList<String> list) {
+        list.addFirst("plum");
+        list.addLast("mango");
+    }
+
+    private static void printEditedList(LinkedList<String> list) {
+        System.out.println("Edited list:");
+        addData(list);
+        getOutput(list);
+    }
+
+    private static void getOutput(List<String> list) {
+        int counter = 1;
+        for (String name : list) {
+            System.out.println(counter + ") " + name);
+            counter++;
+        }
+        System.out.println();
     }
 }
